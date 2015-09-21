003b48f10c9b80baef4600a4b498b3406e1c58fa
Maya Soloveva <soloveva.maya@bk.ru>
2015-09-21 15:11:55 +0300

    В прошлый раз не дозагрузилось

--- js/search-form.js
+++ js/search-form.js
@@ -1,9 +1,11 @@
 var link = document.querySelector(".offer-search-title");
 var form = document.querySelector(".offer-search-form");
 var date_in = form.querySelector("[name=osf-date-in]");
+var btn_search = form.querySelector(".osf-search-btn");
 
 link.addEventListener("click", function (event) {
     event.preventDefault();
     form.classList.toggle("osf-show");
+    btn_search.focus();
     date_in.focus();
 });
